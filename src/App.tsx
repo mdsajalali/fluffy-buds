a92702f61b23b5f876f660da3c7a25d6231b1bc8
mdsajalali <mdsajalali2020@gmail.com>
2025-04-10 20:38:50 +0600

    added: fonts

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ const App = () => {
   return (
     <div>
       <h1 className="text-center">Home Page</h1>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea in modi iste ullam iusto. Dolore dolorum velit sit dicta. Dicta quaerat voluptate similique tenetur voluptas dolores omnis, ratione soluta vitae cum deleniti magnam natus. Dolorem dolor cupiditate consequatur doloribus odio ipsam ipsum maxime amet ipsa? Tempore in cumque repellendus sunt odio hic ex harum mollitia quaerat officia recusandae atque eligendi modi ratione earum eius nesciunt, reprehenderit laboriosam numquam sapiente, natus dolorum voluptate vel! Impedit est maiores blanditiis laborum sapiente! Perspiciatis sint laborum voluptate debitis, facilis minima? Aliquam libero animi aliquid, tempora qui adipisci laboriosam repudiandae dignissimos? Ut laborum itaque repellendus.
     </div>
   );
 };
