7832f50712420878e654609d65d2c5ca14fc7380
mdsajalali <mdsajalali2020@gmail.com>
2025-04-10 20:30:39 +0600

    initial setup

---
+++ README.md
@@ -0,0 +1,1 @@
+# FullStack E-Commerce Website
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-center">Home Page</h1>
+    </div>
+  );
+};
+
+export default App;
